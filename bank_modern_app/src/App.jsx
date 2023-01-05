0c185efbeefe3c0ea7c5d78e73e9fd0394f777eb
Alptekin <alptekin.ocakdan@gmail.com>
2023-01-05 16:34:24 +0300

    Header Section

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import styles from "./style.js";
 
-const App = () =>(
-        <div className="bg-primary w-full overflow-hidden">
-            <h1>Hello, World!</h1>
+import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer} from './components'
+
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Navbar/>
+            </div>
         </div>
-    );
+        <div className={`bg-primary ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Hero/>
+            </div>
+        </div>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Stats/>
+                <Business></Business>
+                <Billing></Billing>
+                <CardDeal></CardDeal>
+                <Testimonials></Testimonials>
+                <Clients></Clients>
+                <CTA></CTA>
+                <Footer></Footer>
+            </div>
+        </div>
+    </div>
+);
 
 export default App;
